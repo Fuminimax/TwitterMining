2fc1db8bcb9f608b3b7f7c3abb70950b73917539
root <root@localhost>
2014-12-15 15:55:21 +0900

    MySQLへの単語登録実装

--- tweets/read_timeline.py
+++ tweets/read_timeline.py
@@ -8,6 +8,7 @@ import re
 import csv
 from pprint import pprint
 from urlparse import ParseResult
+import mysql.connector
 
 CK = 'QFOiO0R9A1oqK94UnjI3Q'
 CS = '9lC3I6cBBNK4TLo6vZFHKpcunJs1FZVGxMAOow8ucs'
@@ -24,6 +25,8 @@ class CustomStreamListener(tweepy.StreamListener):
         dst = re.sub(r'https?://[\w/:%#\$&\?\(\)~\.=\+\-]+', '', txt["text"]) 
         dst = re.sub(r'(?:^|[^ｦ-ﾟー゛゜々ヾヽぁ-ヶ一-龠ａ-ｚＡ-Ｚ０-９a-zA-Z0-9&_\/]+)#([ｦ-ﾟー゛゜々ヾヽぁ-ヶ一-龠ａ-ｚＡ-Ｚ０-９a-zA-Z0-9_]*[ｦ-ﾟー゛゜々ヾヽぁ-ヶ一-龠ａ-ｚＡ-Ｚ０-９a-zA-Z]+[ｦ-ﾟー゛゜々ヾヽぁ-ヶ一-龠ａ-ｚＡ-Ｚ０-９a-zA-Z0-9_]*)', '', dst)
         dst = re.sub(r'@[^\s]+', '', dst)
+        dst = re.sub(r'RT', '', dst)
+        dst = re.sub(r'[!-@[-`{-~]', '', dst)
         print dst
         mc = MeCab.Tagger("-Ochasen")
         en_dst = dst.encode('utf-8')
@@ -31,9 +34,34 @@ class CustomStreamListener(tweepy.StreamListener):
         while parseResult:
             pos = parseResult.feature.split(',')
             if pos[0] != 'BOS/EOS' or parseResult.surface != '' or parseResult.surface != 'RT':
-                if pos[0] == '名詞' and len(unicode(parseResult.surface, 'utf-8')) > 1 :
+                if (pos[0] == '名詞' or pos[0] == '動詞') and len(unicode(parseResult.surface, 'utf-8')) > 1 :
                     print parseResult.surface + ',' + parseResult.feature
-            parseResult = parseResult.next
+                    try:
+                        mysqlcon = mysql.connector.connect(host='192.168.120.29',
+                                                           port = 3306,
+                                                           db = 'TwitterMining',
+                                                           user = 'fumio',
+                                                           passwd = 'fumio1226',
+                                                           charset = 'utf8')
+                        curs = mysqlcon.cursor()
+                        if pos[6] == '*':
+                            word = parseResult.surface
+                        else:
+                            word = pos[6]
+                        curs.execute('SELECT count(*) FROM BagOfWords WHERE words = "' + word + '";') 
+                        cnt = curs.fetchone()
+                        
+                        if cnt[0] >= 1:
+                            curs.execute('UPDATE BagOfWords SET count = count + 1 WHERE words = "' + word + '";')
+                        elif cnt[0] == 0:
+                            curs.execute('INSERT INTO BagOfWords(words, part, count) VALUES ("' + word + '", "' + pos[0] + '",  1);')
+                        else:
+                            print ('Do Not Anything')
+                        
+                        mysqlcon.commit()
+                    except(mysql.connector.errors.ProgrammingError) as e:
+                        print(e)
+                parseResult = parseResult.next
         return True
    
 auth = tweepy.OAuthHandler(CK, CS)
